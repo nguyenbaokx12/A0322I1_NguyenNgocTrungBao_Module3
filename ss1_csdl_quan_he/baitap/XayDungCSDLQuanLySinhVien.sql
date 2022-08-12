2e83dcc0a61afd3dde94ac9a3c60dbc3facb5efd
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2022-08-12 19:50:55 +0700

    up

--- ss1_csdl_quan_he/baitap/XayDungCSDLQuanLySinhVien.sql
+++ ss1_csdl_quan_he/baitap/XayDungCSDLQuanLySinhVien.sql
@@ -1,21 +1,30 @@
 CREATE DATABASE student;
 USE student;
 DROP TABLE class;
+
 CREATE TABLE class(
 id int auto_increment primary key,
 name varchar(50)
-)auto_increment = 1000;
+);
+
+
 CREATE TABLE teacher(
 id int auto_increment primary key,
 name varchar(50),
 age int,
 country varchar(50)
-)auto_increment = 1000;
+);
 
-INSERT INTO class(name)
-values
+INSERT INTO class(id, name)
+VALUES
 ('Trung Bao');
 
-SELECT * FROM class;
+INSERT INTO teacher(name, age, country)
+VALUES
+('Nguyen Van A',32,'Viet Nam');
+
+SELECT * FROM `class`;
+SELECT * FROM teacher;
+
 
 
